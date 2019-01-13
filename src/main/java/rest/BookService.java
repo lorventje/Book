a870f45f8f8fe73fa1e8e0b5b54c8f8dd3e58a2b
Remco <r.vanbrakel@student.fontys.nl>
2019-01-14 00:13:17 +0100

    Working application

--- src/main/java/rest/BookService.java
+++ src/main/java/rest/BookService.java
@@ -1,20 +1,42 @@
 package rest;
 
 import com.google.gson.Gson;
+import com.restcompress.provider.LZF;
+import com.sun.xml.internal.ws.client.sei.ResponseBuilder;
 import domain.Book;
+import org.apache.commons.codec.binary.Base64;
+import org.glassfish.jersey.message.GZipEncoder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.filter.EncodingFilter;
 import persistence.BookMgr;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.Response;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.List;
 import java.util.logging.Logger;
+import java.util.zip.GZIPOutputStream;
 
 @Path("/bookShore")
-public class BookService {
+public class BookService extends ResourceConfig {
     private final Gson gson = new Gson();
     private static final Logger log = Logger.getLogger(BookService.class.getName());
 
     public BookService() { }
+
+    public static String compress(String str) throws IOException {
+        if (str == null || str.length() == 0) {
+            return str;
+        }
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        GZIPOutputStream gzip = new GZIPOutputStream(out);
+        gzip.write(str.getBytes());
+        gzip.close();
+        String outStr = out.toString("UTF-8");
+        return outStr;
+    }
 
     @POST
     @Path("/addBook")
@@ -77,6 +99,8 @@ public class BookService {
         if (books == null){
             return Response.status(400).entity(RestResponseHelper.getErrorResponseString()).build();
         }
-        return Response.status(200).entity(BookResponse.getAllBooks(books)).build();
+        CacheControl cc = new CacheControl();
+        cc.setMaxAge(31536000);
+        return Response.status(200).entity(BookResponse.getAllBooks(books)).cacheControl(cc).build();
     }
 }
